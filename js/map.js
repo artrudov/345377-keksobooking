d3d192d9468d0e27ee7243610c16556e08f54c8e
artrudov <artrudov@gmail.com>
2017-12-10 16:58:18 +0300

    Вывод пинов и карточки описания элемента.

--- js/map.js
+++ js/map.js
@@ -16,10 +16,17 @@ var getMinMaxRandom = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-var getAdwordContent = function () {
+function compareRandom() {
+  return Math.random() - 0.5;
+}
+
+var getAdwordContent = function (numberImage) {
+
   var coordinateX = getMinMaxRandom(300, 900);
   var coordinateY = getMinMaxRandom(100, 500);
-  var urlAvatar = 'img/avatars/user0' + getMinMaxRandom(1, 8) + '.png';
+  var urlAvatar = 'img/avatars/user0' + (numberImage + 1) + '.png';
+  swapFeatures.sort(compareRandom);
+  var features = swapFeatures.slice(0, getRandomIndexArray(swapFeatures) - 1);
 
   return {
     author: {
@@ -35,7 +42,7 @@ var getAdwordContent = function () {
       guests: getMinMaxRandom(1, 100),
       ckeckin: CHECKIN_AND_OUT[getRandomIndexArray(CHECKIN_AND_OUT)],
       checkout: CHECKIN_AND_OUT[getRandomIndexArray(CHECKIN_AND_OUT)],
-      features: FEATURES[getRandomIndexArray(FEATURES)],
+      features: features,
       description: '',
       photos: []
     },
@@ -49,25 +56,79 @@ var getAdwordContent = function () {
 
 var adwords = [];
 for (var i = 0; i < 8; i++) {
-  adwords[i] = getAdwordContent();
+  var swapFeatures = FEATURES;
+
+  adwords[i] = getAdwordContent(i);
+  swapFeatures = FEATURES;
 }
 
 var mapElementsPin = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
-// ОПИСАТЬ ФУНКЦИЮ ДОБАВЛЕНИЯ ЭЛЕМЕНТОВ В КЛОНИРУЕМУЮ НОДУ
+
 var renderPin = function (ads) {
   var pinElement = pinTemplate.cloneNode(true);
   var avatarImageUrl = pinElement.children;
 
-  pinElement.setAttribute('style', 'left:' + ads.location.x +'px;' +' ' + 'top:' + ads.location.y +'px;');
+  pinElement.setAttribute('style', 'left:' + ads.location.x + 'px;' + ' ' + 'top:' + ads.location.y + 'px;');
   avatarImageUrl[0].setAttribute('src', ads.author.avatar);
 
   return pinElement;
 };
 
+
+// Дописать!
+var mapCard = document.querySelector('.map');
+var mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
+
+var renderCard = function (sda) {
+  var cardElement = mapCardTemplate.cloneNode(true);
+  var cardTitle = mapCardTemplate.querySelector('h3');
+  var cardAddress = mapCardTemplate.querySelector('small');
+  var cardPrice = mapCardTemplate.querySelector('.popup__price');
+  var cardType = mapCardTemplate.querySelector('h4');
+  var cardParagraph = mapCardTemplate.querySelectorAll('p');
+  var cardAvatar = mapCardTemplate.querySelector('.popup__avatar');
+  var cardListFeatures = mapCardTemplate.querySelector('.popup__features');
+
+  cardTitle.innerHTML = sda.offer.title;
+  cardAddress.innerHTML = sda.offer.address;
+  cardPrice.innerHTML = sda.offer.price;
+  cardParagraph[2].innerHTML = sda.offer.rooms + ' комнаты для ' + sda.offer.guests + ' гостей';
+  cardParagraph[3].innerHTML = 'Заезд после ' + sda.offer.ckeckin + ', выезд до ' + sda.offer.checkout;
+  cardParagraph[4].innerHTML = sda.offer.description;
+  cardAvatar.setAttribute('src', sda.author.avatar);
+
+  if (sda.offer.type === 'flat') {
+    cardType.innerHTML = 'Квартира';
+  } else if (sda.offer.type === 'bungalo') {
+    cardType.innerHTML = 'Бунгало';
+  } else {
+    cardType.innerHTML = 'Дом';
+  }
+
+  while (cardListFeatures.firstChild) {
+    cardListFeatures.removeChild(cardListFeatures.firstChild);
+  }
+
+  var lengthFeaturesArray = sda.offer.features.length;
+
+  for (var s = 0; s < lengthFeaturesArray; s++) {
+    var newListItem = document.createElement('li');
+    newListItem.className = 'feature feature--' + sda.offer.features[s];
+    cardListFeatures.appendChild(newListItem);
+  }
+
+  return cardElement;
+};
+
 var fragment = document.createDocumentFragment();
-for (var j = 0; j < adwords.length; j++) {
-  fragment.appendChild(renderPin(adwords[j]));
+for (var k = 0; k < adwords.length; k++) {
+  fragment.appendChild(renderPin(adwords[k]));
 }
+
 mapElementsPin.appendChild(fragment);
+
+mapCard.insertBefore(renderCard(adwords[0]), mapCard.children[1]);
+
+mapCard.appendChild(renderCard(adwords[0]));
